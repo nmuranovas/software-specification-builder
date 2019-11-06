fb1333f934d8fe99f8343eb154d3b39df4a8c046
Lawliet <nerijus.muranovas@gmail.com>
2019-11-06 13:15:36 +0200

    Add example test project to backend

---
+++ backend/API.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace API.Tests.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestThatSucceedes()
+        {
+            
+        }
+
+        [Fact]
+        public void TestThatFails()
+        {
+            Assert.True(false);
+        }
+    }
+}
