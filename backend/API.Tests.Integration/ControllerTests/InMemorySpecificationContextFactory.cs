bf54b66541a5948f51f46e051fc5c29f6708f551
Nerijus Muranovas <nerijus.muranovas@gmail.com>
2019-11-12 15:55:55 +0200

    Add Get and Delete integration tests

---
+++ backend/API.Tests.Integration/ControllerTests/InMemorySpecificationContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Tests.Integration.Helpers
+{
+    public static class InMemorySpecificationContextFactory
+    {
+        public static SpecificationContext CreateInMemorySpecificationContext()
+        {
+            var options = new DbContextOptionsBuilder<SpecificationContext>()
+                .UseInMemoryDatabase(databaseName: "test-database")
+                .Options;
+
+            return new SpecificationContext(options);
+        }
+    }
+}
